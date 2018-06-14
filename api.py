c27ef4be72074ecd9604511e5b8d12bad7038b39
Pierre Grassin <pierre.grassin@student.42.fr>
2018-06-14 16:14:23 +0200

    ajout du delet

--- api.py
+++ api.py
@@ -2,17 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import hug
-#import mysql.connector.errors
+# import mysql.connector.errors
 
-from init import connect_db,insert_data  # change pour une lib perso
+from init import connect_db, insert_data  # change pour une lib perso
 import json
 import collections
-
-
-@hug.get('/happy_birthday')
-def happy_birthday(name, age: hug.types.number = 1):
-    """Says happy birthday to a user"""
-    return "Happy {age} Birthday {name}!".format(**locals())
 
 
 @hug.get('/search')
@@ -46,7 +40,7 @@ def search_pokemon_name(name=None, pokemon_id=None, pk_type=None):
             "SELECT `id`, `pokemon_id`, `name`, `Type`, `Type_bis`,`Total`, `HP`, `Attack`, `Defense`, `Sp_attack`, `Sp_defense`, `speed` FROM `pk_database` WHERE 1")
     db.commit()
     if cursor.rowcount == 0:
-        return {"[]"}
+        return
     rows = cursor.fetchall()
     row_list = []
     for row in rows:
@@ -66,27 +60,30 @@ def search_pokemon_name(name=None, pokemon_id=None, pk_type=None):
     # row_list = json.dumps(row_list)
     return row_list
 
+
 @hug.post('/change')
-def new_pokemon(id=None, pokemon_id=None, name=None, type=None, type_bis=None, total=None, hp=None, attack=None, defense=None, sp_attack=None, sp_defense=None, speed=None):
+def new_pokemon(id=None, pokemon_id=None, name=None, type=None, type_bis=None, total=None, hp=None, attack=None,
+                defense=None, sp_attack=None, sp_defense=None, speed=None):
     db = connect_db()
     cursor = db.cursor()
 
     if id is None:
-        return {"Error":"You need an id to update a pokemon"}
+        return {"Error": "You need an id to update a pokemon"}
     cursor.execute("SELECT `name` FROM `pk_database` WHERE `id` = " + id + "")
     cursor.fetchall()
     if cursor.rowcount is not 1:
-        return {"Error":"No pokemon in database with this id"}
+        return {"Error": "No pokemon in database with this id"}
 
     if name is not None:
-        cursor.execute("SELECT `id` FROM `pk_database` WHERE `name` = '" + name + "'") #on cherche à savoir si il y a un pokemon (autre que celui id) qui as le meme nom
+        cursor.execute(
+            "SELECT `id` FROM `pk_database` WHERE `name` = '" + name + "'")  # on cherche à savoir si il y a un pokemon (autre que celui id) qui as le meme nom
         cursor.fetchall()
         if cursor.rowcount is not 0:
-            return {"Error":"A pokemon have already this name"}
+            return {"Error": "A pokemon have already this name"}
 
-##CHECK VALIDE TYPE
-##TYPE DIFFERENT
-##
+    ##CHECK VALIDE TYPE
+    ##TYPE DIFFERENT
+    ##
     if type is not None:
         cursor.execute("SELECT `id` FROM `pk_type` WHERE `type` = '" + type + "'")
         cursor.fetchall()
@@ -100,9 +97,9 @@ def new_pokemon(id=None, pokemon_id=None, name=None, type=None, type_bis=None, t
     if type == type_bis and type is not None:
         return {"Error": "The types can't be the same"}
 
-
     # pour les none value on leurs affecte la value de base
-    cursor.execute("SELECT `id`, `pokemon_id`, `name`, `Type`, `Type_bis`,`Total`, `HP`, `Attack`, `Defense`, `Sp_attack`, `Sp_defense`, `speed` FROM `pk_database` WHERE `id` = " + id)
+    cursor.execute(
+        "SELECT `id`, `pokemon_id`, `name`, `Type`, `Type_bis`,`Total`, `HP`, `Attack`, `Defense`, `Sp_attack`, `Sp_defense`, `speed` FROM `pk_database` WHERE `id` = " + id)
     row = cursor.fetchone()
     if pokemon_id is None:
         pokemon_id = row[1]
@@ -131,9 +128,13 @@ def new_pokemon(id=None, pokemon_id=None, name=None, type=None, type_bis=None, t
     if speed is None:
         speed = row[11]
 
-    cursor.execute("UPDATE `pk_database` SET `pokemon_id`="+str(pokemon_id)+",`name`='"+name+"',`Type`='"+type+"',`Type_bis`='"+type_bis+"',`Total`="+str(total)+",`HP`="+str(hp)+",`Attack`="+str(attack)+",`Defense`="+str(defense)+",`Sp_attack`="+str(sp_attack)+",`Sp_defense`="+str(sp_attack)+",`speed`="+str(speed)+" WHERE `id`="+str(id))
+    cursor.execute("UPDATE `pk_database` SET `pokemon_id`=" + str(
+        pokemon_id) + ",`name`='" + name + "',`Type`='" + type + "',`Type_bis`='" + type_bis + "',`Total`=" + str(
+        total) + ",`HP`=" + str(hp) + ",`Attack`=" + str(attack) + ",`Defense`=" + str(defense) + ",`Sp_attack`=" + str(
+        sp_attack) + ",`Sp_defense`=" + str(sp_defense) + ",`speed`=" + str(speed) + " WHERE `id`=" + str(id))
     db.commit()
-    cursor.execute("SELECT `id`, `pokemon_id`, `name`, `Type`, `Type_bis`,`Total`, `HP`, `Attack`, `Defense`, `Sp_attack`, `Sp_defense`, `speed` FROM `pk_database` WHERE `id` = " + id)
+    cursor.execute(
+        "SELECT `id`, `pokemon_id`, `name`, `Type`, `Type_bis`,`Total`, `HP`, `Attack`, `Defense`, `Sp_attack`, `Sp_defense`, `speed` FROM `pk_database` WHERE `id` = " + id)
     row = cursor.fetchone()
     d = collections.OrderedDict()
     d['id'] = row[0]
@@ -150,6 +151,7 @@ def new_pokemon(id=None, pokemon_id=None, name=None, type=None, type_bis=None, t
     cursor.close()
     return d
 
+
 @hug.delete("/delete")
 def delete_pokemon(id):
     try:
@@ -162,6 +164,7 @@ def delete_pokemon(id):
     cursor.execute("DELETE FROM `pk_database` WHERE `id` =" + id)
     db.commit()
 
+
 @hug.get("/purge")
 def purge_database():
     db = connect_db()
@@ -169,3 +172,59 @@ def purge_database():
     cursor.execute("DROP TABLE IF EXISTS `pk_database`, `pk_type`;")
     db.commit()
     insert_data()
+
+
+@hug.post("/create")
+def create_pokemon(pokemon_id=None, name=None, total=None, hp=None, attack=None, defense=None, sp_attack=None,
+                   sp_defense=None, speed=None, type=None, type_bis=None):
+    db = connect_db()
+    cursor = db.cursor()
+
+    if name is None:
+        return {"Error": "You need a name"}
+    if name is not None:
+        cursor.execute(
+            "SELECT `id` FROM `pk_database` WHERE `name` = '" + name + "'")  # on cherche à savoir si il y a un pokemon (autre que celui id) qui as le meme nom
+        cursor.fetchall()
+        if cursor.rowcount is not 0:
+            return {"Error": "A pokemon have already this name"}
+
+    ##CHECK VALIDE TYPE
+    ##TYPE DIFFERENT
+    ##
+    if type is not None:
+        cursor.execute("SELECT `id` FROM `pk_type` WHERE `type` = '" + type + "'")
+        cursor.fetchall()
+        if cursor.rowcount is not 1:
+            return {"Error": "The type " + type + " don't exist"}
+    if type_bis is not None:
+        cursor.execute("SELECT `id` FROM `pk_type` WHERE `type` = '" + type_bis + "'")
+        cursor.fetchall()
+        if cursor.rowcount is not 1:
+            return {"Error": "The type" + type_bis + " don't exist"}
+    if type == type_bis and type is not None:
+        return {"Error": "The types can't be the same"}
+
+    data = [int(pokemon_id), name, int(total), int(hp), int(attack), int(defense), int(sp_attack), int(sp_defense),
+            int(speed), type, type_bis]
+
+    cursor.execute("""INSERT INTO `pk_database`(`pokemon_id`, `name`, `Total`, `HP`, `Attack`,
+            `Defense`, `Sp_attack`, `Sp_defense`, `speed`, `type`, `type_bis`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                   data)
+    db.commit()
+    cursor.execute(
+        "SELECT `id`, `pokemon_id`, `name`, `Type`, `Type_bis`,`Total`, `HP`, `Attack`, `Defense`, `Sp_attack`, `Sp_defense`, `speed` FROM `pk_database` WHERE `id` = " + str(cursor.lastrowid))
+    row = cursor.fetchone()
+    d = collections.OrderedDict()
+    d['id'] = row[0]
+    d['pokemon_id'] = row[1]
+    d['name'] = row[2]
+    d['Type'] = [row[3], row[4]]
+    d['Total'] = row[5]
+    d['HP'] = row[6]
+    d['Attack'] = row[7]
+    d['Defense'] = row[8]
+    d['Sp_attack'] = row[9]
+    d['Sp_defense'] = row[10]
+    d['speed'] = row[11]
+    return d
